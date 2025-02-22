350d387d0fb626833076c15ae75ba2dbc59ea49e
MihVieira <mih.vieira1@gmail.com>
2025-02-22 17:33:39 -0400

    add controllers, services, and exception handling for user management

---
+++ src/main/java/com/example/api_rest/controller/NewsController.java
@@ -0,0 +1,4 @@
+package com.example.api_rest.controller;
+
+public class NewsController {
+}
---
+++ src/main/java/com/example/api_rest/service/impl/UserServiceImpl.java
@@ -0,0 +1,28 @@
+package com.example.api_rest.service.impl;
+
+import com.example.api_rest.domain.model.User;
+import com.example.api_rest.domain.repository.UserRepository;
+import com.example.api_rest.service.UserService;
+import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public User findById(Long id) {
+        return userRepository.findById(id).orElseThrow(NoSuchElementException::new);
+    }
+
+    @Override
+    public User create(User userToCreate) {
+        return userRepository.save(userToCreate);
+    }
+}
